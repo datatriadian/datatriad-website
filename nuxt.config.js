5bed784b99811b41a63e767b9c7b6a65ee4612d9
iLevyTate <78818969+iLevyTate@users.noreply.github.com>
2021-03-24 20:47:31 -0400

    updated the title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'datatriad-website',
+    title: 'Data Triad',
     htmlAttrs: {
       lang: 'en',
     },
