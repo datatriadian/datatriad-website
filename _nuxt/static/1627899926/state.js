45fb77a67dbeae23d7612e99b01b9257c428b4f3
sanders41 <sanders41@users.noreply.github.com>
2021-08-03 09:39:01 +0000

    deploy: 202df7f02dfa8ea9bcc24afe5e17e7d4ff1c872b

--- _nuxt/static/1627899926/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1627899926",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b49d9fd0"}}}}(null,"\u002F"));
