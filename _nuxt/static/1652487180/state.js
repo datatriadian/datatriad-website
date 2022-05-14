dbcc6e64889e69cd6a84dd1e25c6704db13954f0
sanders41 <sanders41@users.noreply.github.com>
2022-05-14 00:22:14 +0000

    deploy: d5e482f8a87883b8c4a24e83ab157a071b26510b

--- _nuxt/static/1652487180/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdatatriad-website\u002F_nuxt\u002Fstatic\u002F1652487180",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fdatatriad-website\u002F",assetsPath:"\u002Fdatatriad-website\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"89e51de8"}}}}(null));
---
+++ _nuxt/static/1652487733/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdatatriad-website\u002F_nuxt\u002Fstatic\u002F1652487733",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fdatatriad-website\u002F",assetsPath:"\u002Fdatatriad-website\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c5f0bbf6"}}}}(null));
