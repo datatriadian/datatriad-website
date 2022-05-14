b8605fd0b384fc8e5d654909bb2faf6eca07ec7b
Paul Sanders <psanders1@gmail.com>
2022-05-13 20:07:50 -0400

    d9ba2e7

---
+++ _nuxt/static/1652486862/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdatatriad-website\u002F_nuxt\u002Fstatic\u002F1652486862",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fdatatriad-website\u002F",assetsPath:"\u002Fdatatriad-website\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a1d752d2"}}}}(null));
