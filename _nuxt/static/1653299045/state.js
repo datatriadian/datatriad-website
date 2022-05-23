c24efd6d0945b1bbffabd8f6d06d85c73d2c4d55
sanders41 <sanders41@users.noreply.github.com>
2022-05-23 09:44:08 +0000

    deploy: 4b1a23d42c7d220b4048dad9564dcd3c4e28c1e5

--- _nuxt/static/1653299045/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdatatriad-website\u002F_nuxt\u002Fstatic\u002F1653299045",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fdatatriad-website\u002F",assetsPath:"\u002Fdatatriad-website\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0554c9e8"}}}}(null));
